c51cb29a996644271b0f47f2053ed9cab1cbd5ce
Feyza-Balaban <feyzablbn@hotmail.com>
2025-04-03 11:13:57 +0200

    Create python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,35 @@
+# Phayton variables
+age = 30
+print("Integer:", age)
+
+# Float 
+height = 1.75
+print("Float:", height)
+
+# String 
+language = "Pythonista"
+print("String:", language)
+
+# List 
+colors = ["red", "green", "blue"]
+print("List:", colors)
+
+# Tuple 
+location = (12, 8)
+print("Tuple:", location)
+
+# Dictionary 
+student = {"first_name": "John", "grade": 11}
+print("Dictionary:", student)
+
+# Boolean 
+has_passed = False
+print("Boolean:", has_passed)
+
+# Set 
+lucky_numbers = {7, 14, 21}
+print("Set:", lucky_numbers)
+
+# Type Checking
+print("Type of age:", type(age))
+print("Type of language:", type(language))
